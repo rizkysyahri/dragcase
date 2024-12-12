64595223eb5ef27837c25a645f18da03c114c970
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2024-12-12 18:54:23 +0700

    fix: cors configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "utfs.io"],
   },
+  crossOrigin: "anonymous",
 
   async headers() {
     return [
@@ -11,10 +12,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://dragcase.vercel.app" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
