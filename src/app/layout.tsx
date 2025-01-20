e76d4f1aac1af5748cd29be475ca597e77f5819f
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2025-01-21 00:37:40 +0700

    fix(auth): adding kinde provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
+    <Providers>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
 
-        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
-          <div className="flex flex-1 flex-col h-full">
-            <Providers>{children}</Providers>
-          </div>
-          <Footer />
-        </main>
-        <Toaster />
-      </body>
-    </html>
+          <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+            <div className="flex flex-1 flex-col h-full">{children}</div>
+            <Footer />
+          </main>
+          <Toaster />
+        </body>
+      </html>
+    </Providers>
   );
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
 const client = new QueryClient();
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <KindeProvider>
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </KindeProvider>
+  );
 };
 
 export default Providers;
